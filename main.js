52b128b0aaf94b2272cb758640a85b8ef97fa0ab
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-01-24 09:09:24 +0000

    new

---
+++ main.js
@@ -0,0 +1,30 @@
+class Navbar {
+    constructor(target, menu) {
+        if (target instanceof HTMLElement && menu instanceof HTMLElement) {
+            this.btn = target;
+            this.menu = menu;
+
+            this.btn.addEventListener('click', () => {
+                this.open();
+            });
+        } else {
+            throw new TypeError("The Target and Menu arguments must be a DOM element."); 
+        }
+        
+    }
+
+    open() {
+        if (this.isopen) {
+            this.menu.classList.add('nav-hidden');
+        } else {
+            this.menu.classList.remove('nav-hidden');
+        }
+
+        this.isopen = !this.isopen;
+    }
+}
+
+const navbar = new Navbar(
+    document.querySelector(".menu-btn"),
+    document.querySelector(".menu-list")
+);
